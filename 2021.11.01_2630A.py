d4c1e1e20e7af5b5c53709d7a66c45ecaea27272
starsil <kimse9811@naver.com>
2021-11-05 17:34:40 +0900

    색종이 만들기

--- 2021.11.01_2630A.py
+++ 2021.11.01_2630A.py
@@ -1,27 +1,31 @@
+#색종이 만들기
 import sys
-input = sys.stdin.readline
 n = int(input())
-B = [list(map(int, input().split())) for _ in range(n)]
-w_cnt, b_cnt = 0, 0
+B = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+White, Blue = 0, 0
 
-def div_conq(x, y, N):
-    global w_cnt, b_cnt
-    tmp_cnt = 0
+def Distinguish(x, y, N):
+    global White, Blue
+    tmp = 0
     for i in range(x, x + N):
         for j in range(y, y + N):
+            #행렬이 1일 경우(파란색)
             if B[i][j]:
-                tmp_cnt += 1
-    if not tmp_cnt:
-        w_cnt += 1
-    elif tmp_cnt == N**2:
-        b_cnt += 1
+                tmp += 1
+    #모든 행이 0일경울(하얀색)
+    if not tmp:
+        White += 1
+    #모든 행렬이 1일 경우(파란색)
+    elif tmp == N**2:
+        Blue += 1
+    #행령에 0과 1이 섞여있는 경우 4등분 한다.
     else:
-        div_conq(x, y, N // 2)
-        div_conq(x + N // 2, y, N // 2)
-        div_conq(x, y + N // 2, N // 2)
-        div_conq(x + N // 2, y + N // 2, N // 2)
+        Distinguish(x, y, N // 2)
+        Distinguish(x + N // 2, y, N // 2)
+        Distinguish(x, y + N // 2, N // 2)
+        Distinguish(x + N // 2, y + N // 2, N // 2)
     return
 
-div_conq(0, 0, n)
-print(w_cnt)
-print(b_cnt)
+Distinguish(0, 0, n)
+print(White)
+print(Blue)
