740a3686ff9f74f023f1790a3ee7bfeed3ece392
star-sil <kimse9811@naver.com>
2021-11-06 15:19:39 +0900

    테스트

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(void)
+{
+    std::cout << "asdf";
+    return 0;
+}
+//전부 안해도 되고, cpp 만 할거면 cpp 부분 한 줄만 추가해도 된다.
+// 핵심은 g++ -std=c++14(버전) 이다.
+// 이 부분이 C++ 버전을 정해준다.
