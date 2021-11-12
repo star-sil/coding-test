f25a9b4bfd6279e6e595d6d2ce90bf6fc4ee794a
star-sil <kimse9811@naver.com>
2021-11-12 16:21:34 +0900

    test

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-if 'h' in 'hello':
-    print(1)
+class hello:
+    def __init__(self,count):
+        self.count = count
+        pass
+    def fprint(self):
+        print(self.count)
+li = []
+for i in range(1,4):
+    a = hello(i)
+    li.append(a)
+print(li)
+
+for i in li:
+    i.fprint()
