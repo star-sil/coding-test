e5598d5a4b9362420818e81efbec303e12afce7d
star-sil <kimse9811@naver.com>
2022-01-19 13:29:33 +0900

    변경사항 추가

--- 9576A.cpp
+++ 9576A.cpp
@@ -2,6 +2,7 @@
 //반례를 찾지 못함
 //테스트 케이스에서 초기화를 시켜주지 못했다.
 //visited 배열의 길이가 1000이 아니라 1001이되어야한다.
+//웬만하면 전역변수로 선언하는게 좋다. 속도 증가
 //size 잘못 찾았다.
 #include <algorithm>
 #include <vector>
@@ -11,6 +12,7 @@ using namespace std;
 
 typedef pair<int,int> ii;
 typedef vector<ii> vii;
+vii AL;
 int arr[1001];
 int ct, T;
 
@@ -18,28 +20,24 @@ bool compare(ii a, ii b)
 {
     if (!(a.second == b.second))
         return a.second < b.second;
-
     return a.first < b.first;
 }
 
 int main(){
     scanf("%d", &T);
-    
     for(int i = 0; i < T; ++i)
     {
-        int M, N; scanf("%d %d",&N,&M);
-        vii AL;
-        memset(arr,0,sizeof(arr));
+        vii AL; int M, N; scanf("%d %d",&N,&M);
+        //배열과 벡터 초기화
+        memset(arr,0,sizeof(arr)); AL.assign(M,ii(0,0));
         for(int j = 0; j < M; ++j){
             int a, b; scanf("%d %d", &a, &b);
-            AL.push_back(ii(a,b));
+            AL[j] = ii(a,b);
         }
         sort(AL.begin(),AL.end(),compare);
         for(int j = 0; j < M; ++j)
         {
-            int n = AL[j].first;
-            int m = AL[j].second;
-            for(int k = n; k <= m; ++k){
+            for(int k = AL[j].first; k <= AL[j].second; ++k){
                 if(arr[k] == 0){
                     ct++;
                     arr[k] = 1;
