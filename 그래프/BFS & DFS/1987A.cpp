e14a1324aa8e137e6d2172ea2532ce46aa1bb68c
star-sil <kimse9811@naver.com>
2022-02-19 20:00:28 +0900

    알파벳

--- 그래프/BFS & DFS/1987A.cpp
+++ 그래프/BFS & DFS/1987A.cpp
@@ -4,6 +4,7 @@
 다음 방문을 하려하기 전에 방문했다고 처리하고 다시 방문을 안했다고 설정한다..
 
 ++
+
 graph는 전역 변수 이므로 모든 노드들이 방문여부를 공유한다. 따라서 하나의 노드가 방문해서 방문 처리를하면
 다른 노드는 방문을 안했어도 해당 좌표에 방문을 한 것으로 된다.
 **/
@@ -20,11 +21,11 @@ void dfs(int x, int y, int cnt){
     result = (cnt > result) ? cnt : result;
 
     if(x < R && x >= 0 && y < C && y >= 0 && visited[index] && graph[x][y] != 0){
-        cnt++; alpha[Index] = true;
+        cnt++; visited[index] = true;
         
         dfs(x+1,y,cnt); dfs(x,y+1, cnt); dfs(x-1,y, cnt); dfs(x,y-1,cnt);
         
-        alpha[Index] = false;
+        visited[index] = false;
     }
 }
 
