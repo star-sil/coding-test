4bfcf83b29a40c38fe23406763cff6e4b51cf8af
star-sil <kimse9811@naver.com>
2022-04-02 23:22:17 +0900

    택배

--- 그리디/8980T.cpp
+++ 그리디/8980T.cpp
@@ -38,10 +38,13 @@ int main(){
         int start = boxes[i].first;
         int end = boxes[i].second.first;
         int cap = boxes[i].second.second;
+
+        //배달 하는 마을을 부터 목적지까지 쌓인 택배수 확인(가장 큰 것 선택)
         for(int j = start; j < end; ++j){
             maxBox = max(maxBox, truck[j]);
         }
 
+        //용량 - 현재 경로에 걸치는 마을중 가장 큰 택배수 만큼 가져갈 수 있다.
         maxBox = min(C-maxBox,cap);
         result += maxBox;
 
