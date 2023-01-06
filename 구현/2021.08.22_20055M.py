7a88e455778c97d06d5c29c8004cae8a08aba0de
star-sil <kimse9811@naver.com>
2023-01-06 15:59:05 +0900

    BOJ 컨베이어 벨트 위의 로봇

--- 구현/2021.08.22_20055M.py
+++ 구현/2021.08.22_20055M.py
@@ -1,74 +1,51 @@
-import sys
-N, K = map(int,sys.stdin.readline().split())
-A = list(map(int,sys.stdin.readline().split()))
-A_ = list()
-for i in range(len(A)):
-    if i == 0:
-        A_.append([A[i],0,'u'])
-    elif i == N-1:
-        A_.append([A[i],0,'d'])
-    else:    
-        A_.append([A[i],0,'n'])
-A = A_
-#회전 시키기
-u_pos = d_pos = 0
-def lotation(a):
-    #위치 탐색
-    global u_pos, d_pos
-    for i in range(len(a)):
-        if a[i][2] == 'u':
-            a[i][2] = 'n'
-            u_pos = i-1
-        elif a[i][2] == 'd':
-            a[i][2] = 'n'
-            d_pos = i-1
-        else:
-            continue
-    a[u_pos][2] = 'u'
-    a[d_pos][2] = 'd'
-    if a[d_pos][1] == 1:
-        a[d_pos][1] = 0
-    return a
-#로봇 이동시키기
-def move(a):
-    for i in range(N*2-1,-1,-1):
-        if a[i][1] == 1:
-            pos = i
-            n_pos = (i+1) % (2*N)
-            if a[n_pos][0] != 0 and a[n_pos][1] != 1:
-                #발판이 내리는 곳일 경우 아닌경우
-                if a[n_pos][2] == 'd':
-                    a[pos][1] = 0
-                    a[n_pos][0] -= 1
-                    a[n_pos][1] = 0
-                else:
-                    a[pos][1] = 0
-                    a[n_pos][0] -= 1
-                    a[n_pos][1] = 1
-    return a
+# 컨베이어 벨트 위의 로봇
+from collections import deque
 
-def count_zero(a):
-    count = 0
-    for i in a:
-        if i[0] == 0:
-            count += 1
-    return count
+n, k = map(int, input().split())
 
+belts = list(map(int,input().split()))
+robots = [False] * n
 
-count = 0
+ans = 0
 while True:
-    count += 1
-    A = lotation(A)
-    A = move(A)
-    if A[u_pos][0] != 0:
-        A[u_pos][0] -= 1
-        A[u_pos][1] = 1
+    
+    ans += 1
 
-    if count_zero(A) >= K:
-        break
-print(count)
+    # 벨트 & 로봇 회전
+    tmp = [0] * 2 * n
+    for i in range(2*n):
+        tmp[(i+1) % (2*n)] = belts[i]
+    belts = tmp
 
-         
+    tmp = [0] * len(robots)
+    for i in range(len(robots)):
+        tmp[(i+1) % len(robots)] = robots[i]
+    robots = tmp
 
-        
+    # 내리는 위치에 있는 로봇 제거
+    if robots[n-1]:
+        robots[n-1] = False
+    
+    # 로봇 이동 가장 먼저 올라간 로봇부터 이동
+    for i in range(len(robots)-2,-1,-1):
+        if robots[i] and not robots[i+1] and belts[i+1] > 0:
+            robots[i+1] = True
+            robots[i] = False
+            belts[i+1] -= 1
 
+            if i+1 == n-1:
+                robots[i+1] = False
+    
+    # 로봇 올리기
+    if not robots[0] and belts[0] > 0:
+        belts[0] -= 1
+        robots[0] = True
+    
+    cnt = 0
+    for belt in belts:
+        if belt == 0:
+            cnt += 1
+        if cnt >= k:
+            print(ans)
+            exit()
+    
