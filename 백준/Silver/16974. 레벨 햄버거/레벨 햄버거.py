f63cfaa23dd59b02bbec7dbb6895ac7bb47e3e5b
김성은 <70811575+star-sil@users.noreply.github.com>
2023-01-19 17:03:51 +0900

    [Silver I] Title: 레벨 햄버거, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
@@ -0,0 +1,30 @@
+n, x = map(int,input().split())
+d = [0]*(n+1)
+p = [0]*(n+1)
+d[0] = 1
+p[0] = 1
+
+for i in range(1, n+1):
+    # d 배열: i 레벨 햄버거 크기 (햄버거번 + 패티)
+    d[i] = 1 + d[i-1] + 1 + d[i-1] + 1
+    # P 배열: i 레벨 햄버거 패티 개수
+    p[i] = p[i-1] + 1 + p[i-1]
+
+def go(n, x):
+    if n == 0:
+        if x == 0:
+            return 0
+        else:
+            return 1
+    elif x == 1:
+        return 0
+    elif x <= 1 + d[n-1]:
+        return go(n-1, x-1)
+    elif x == 1 + d[n-1] + 1:
+        return p[n-1] + 1
+    elif x <= 1 + d[n-1] + 1 + d[n-1]:
+        return p[n-1] + 1 + go(n-1, x-1-d[n-1]-1)
+    else:
+        return p[n-1] + 1 + p[n-1]
+
+print(go(n,x))
